7125ad5a557b894016a89a5c34b7242193de4b99
dongdukgo <dongdukgo@naver.com>
2019-06-29 18:38:05 +0900

    first commit

---
+++ cctv/__init__.py
@@ -0,0 +1,13 @@
+from cctv.controller import CCTVController
+
+from cctv.data_reader import DataReader
+
+if __name__ == '__main__':
+
+    ctrl = CCTVController()
+
+    ctrl.test()
+
+    # dr = DataReader()
+
+    # print(dr.create_gmaps())
